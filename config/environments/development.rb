7566af24e71cd27a60a87e8b93f961611c5b219f
Lucas Churchill <lucas@agtlucas.com>
2014-08-16 00:30:09 -0300

    Updates..

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,8 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    password: ENV["GMAIL_PASSWORD"],
+    openssl_verify_mode: 'none'
   }
 
   # Print deprecation notices to the Rails logger.
